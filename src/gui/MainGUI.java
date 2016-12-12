be7d244f748b626f045fffb676f98f69de4bcbd2
Mickhopes <mickael.turnel@gmail.com>
2016-12-12 21:34:10 +0100

    Fixed pattern not entirely correct. Fixed some useless imports.

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
